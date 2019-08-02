c461c1c9f5474f254e40932862f6c1feeae8d0c0
yeramwap <priyanka.yeramwar@travelex.com>
2019-08-02 15:48:17 +0530

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = 'abc123!';
+
+bcrypt.genSalt(15,(err,salt) =>{
+    bcrypt.hash(password,salt,(err,hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword ='$2a$15$72mS3mzKHVckNEhkpiAn6O410tpPnh/DEfvWUAuqIIdSxpSsi6g0G';
+
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+});
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
@@ -26,13 +41,13 @@ const jwt = require('jsonwebtoken');
 // }
 
 
-var data ={
-    id: 12
-};
+// var data ={
+//     id: 12
+// };
 
-var token = jwt.sign(data,'123xyz');
-console.log(token);
+// var token = jwt.sign(data,'123xyz');
+// console.log(token);
 
 
-var decoded = jwt.verify(token,'123xyz');
-console.log('decoded',decoded);
+// var decoded = jwt.verify(token,'123xyz');
+// console.log('decoded',decoded);
